23d02f3d2b8c0c0ca4ada4d9329a9d796585d3c7
Pavel Zaneuski <Pavel_Zaneuski@epam.com>
2022-03-25 20:53:03 +0300

    feat: implement task 4

---
+++ src/ExceptionHandling/Task3/Exceptions/DuplicateTaskException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task3.Exceptions
+{
+    public class DuplicateTaskException : Exception
+    {
+        public DuplicateTaskException()
+        {
+        }
+
+        public DuplicateTaskException(string? message) : base(message)
+        {
+        }
+
+        public DuplicateTaskException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
--- src/ExceptionHandling/Task3/UserTaskController.cs
+++ src/ExceptionHandling/Task3/UserTaskController.cs
@@ -1,4 +1,6 @@
-﻿using Task3.DoNotChange;
+﻿using System;
+using Task3.DoNotChange;
+using Task3.Exceptions;
 
 namespace Task3
 {
@@ -25,16 +27,23 @@ namespace Task3
 
         private string GetMessageForModel(int userId, string description)
         {
-            var task = new UserTask(description);
-            int result = _taskService.AddTaskForUser(userId, task);
-            if (result == -1)
-                return "Invalid userId";
-
-            if (result == -2)
-                return "User not found";
-
-            if (result == -3)
-                return "The task already exists";
+            try
+            {
+                var task = new UserTask(description); 
+                _taskService.AddTaskForUser(userId, task);
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                return e.Message;
+            }
+            catch (UserNotFoundException e)
+            {
+                return e.Message;
+            }
+            catch (DuplicateTaskException e)
+            {
+                return e.Message;
+            }
 
             return null;
         }
