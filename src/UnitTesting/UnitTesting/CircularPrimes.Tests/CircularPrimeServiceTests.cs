4c6af1e31ac04782bc9890891a7a662e484116a0
Pavel Zaneuski <Pavel_Zaneuski@epam.com>
2022-04-23 18:36:14 +0300

    feat: add test for CircularPrime Service

---
+++ src/UnitTesting/UnitTesting/CircularPrimes.Tests/CircularPrimeServiceTests.cs
@@ -0,0 +1,42 @@
+using NUnit.Framework;
+
+namespace CircularPrimes.Tests
+{
+    public class Tests
+    {
+        [Test]
+        [TestCase(1, 0)]
+        [TestCase(10, 4)]
+        [TestCase(100, 13)]
+        [TestCase(1000, 25)]
+        [TestCase(10000, 33)]
+        [TestCase(100000, 43)]
+        [TestCase(1000000, 55)]
+        public void GetCircularPrimesAmount_Range_ReturnsAmountOfCircularPrimes(int range, int expectedAmount)
+        {
+            // Arrange
+            var circularPrimeService = new CircularPrimeService();
+
+            // Act
+            var actualAmount = circularPrimeService.GetCircularPrimesAmount(range);
+
+            // Assert
+            Assert.AreEqual(expectedAmount, actualAmount);
+        }
+
+        [Test]
+        [TestCase(1, new int[] { })]
+        [TestCase(10, new[] { 2, 3, 5, 7 })]
+        public void GetCircularPrimes_Number_ReturnsCircularPrimes(int range, int[] expectedRange)
+        {
+            // Arrange
+            var circularPrimeService = new CircularPrimeService();
+
+            // Act
+            var actualRange = circularPrimeService.GetCircularPrimes(range);
+
+            // Assert
+            Assert.AreEqual(expectedRange, actualRange);
+        }
+    }
+}
