9f46936fb78912170a9a4d286d9ec98617f9fc5c
Pavel Zaneuski <Pavel_Zaneuski@epam.com>
2022-04-23 20:34:47 +0300

    feat: add test cases for calc stats

---
+++ src/UnitTesting/UnitTesting/CalcStats.Tests/CalcStatsServiceTests.cs
@@ -0,0 +1,71 @@
+using NUnit.Framework;
+
+namespace CalcStats.Tests
+{
+    public class CalcStatsServiceTests
+    {
+        [Test]
+        [TestCase(new[] { 0, 1, 2 }, 0)]
+        [TestCase(new[] { -6, 7, -21 }, -21)]
+        [TestCase(new[] { 1234, -4567, 8910 }, -4567)]
+        public void GetMinValue_ArrayOfIntegers_ReturnsMinValue(int[] sequence, int expectedMin)
+        {
+            // Arrange
+            var calcStatsServiceService = new CalcStatsService();
+
+            // Act
+            var actualMin = calcStatsServiceService.GetMinValue(sequence);
+
+            // Assert
+            Assert.AreEqual(expectedMin, actualMin);
+        }
+
+        [Test]
+        [TestCase(new[] { 0, 1, 2 }, 2)]
+        [TestCase(new[] { -6, -7, -21 }, -6)]
+        [TestCase(new[] { 1234, -4567, 8910 }, 8910)]
+        public void GetMaxValue_ArrayOfIntegers_ReturnsMaxValue(int[] sequence, int expectedMax)
+        {
+            // Arrange
+            var calcStatsService = new CalcStatsService();
+
+            // Act
+            var actualMax = calcStatsService.GetMaxValue(sequence);
+
+            // Assert
+            Assert.AreEqual(expectedMax, actualMax);
+        }
+
+        [Test]
+        [TestCase(new[] { 123456 }, 1)]
+        [TestCase(new[] { 6, 7, 8 }, 3)]
+        [TestCase(new[] { -6, -7, -21, 1234, -4567, 8910 }, 5)]
+        public void GetSequenceCount_ArrayOfIntegers_ReturnsCountOfSequence(int[] sequence, int expectedCount)
+        {
+            // Arrange
+            var calcStatsService = new CalcStatsService();
+
+            // Act
+            var actualCount = calcStatsService.GetSequenceCount(sequence);
+
+            // Assert
+            Assert.AreEqual(expectedCount, actualCount);
+        }
+
+        [Test]
+        [TestCase(new[] { 0, 1, 2 }, 1)]
+        [TestCase(new[] { -6, 7, -22 }, -7)]
+        [TestCase(new[] { 1234, -4567, 8910 }, 1.859)]
+        public void GetAverageValue_ArrayOfIntegers_ReturnsAverageValue(int[] sequence, double expectedAverage)
+        {
+            // Arrange
+            var calcStatsService = new CalcStatsService();
+
+            // Act
+            var actualAverage = calcStatsService.GetAverageValue(sequence);
+
+            // Assert
+            Assert.AreEqual(expectedAverage, actualAverage);
+        }
+    }
+}
